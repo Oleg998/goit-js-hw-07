4ea10f9622e28a03f16bfc01a1653c1cc31e6b4e
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-11 13:10:39 +0200

    2.1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const contaiter = document.querySelector(".gallery");
+const markup = galleryItems.map(
+  ({ preview, description, original }) =>
+    `<li class="gallery__item"><a class="gallery__link"  href="${original}"><img class="gallery__image" src="${preview}"  alt="${description}"></a></li> `
+);
+contaiter.insertAdjacentHTML("beforeend", markup.join(" "));
