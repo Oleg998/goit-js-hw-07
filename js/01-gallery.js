1c7ba24a57ca3c718a746711edf6b67c6b52c311
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-11 12:51:57 +0200

    01done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const contaiter = document.querySelector(".gallery");
+const markup = galleryItems.map(
+  ({ preview, description, original }) =>
+    `<li class="gallery__item"><a class="gallery__link"  href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></li> `
+);
+contaiter.insertAdjacentHTML("beforeend", markup.join(" "));
+contaiter.addEventListener("click", onClick);
 
-console.log(galleryItems);
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const galleryID = evt.target.dataset.source;
+  const descriptionID = evt.target.getAttribute("alt");
+  const instance = basicLightbox.create(`
+    <div class="modal"> 
+    <img src="${galleryID}" alt="${descriptionID}">
+    </div>
+`);
+  instance.show();
+  contaiter.addEventListener("keydown", onKey);
+  function onKey(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
